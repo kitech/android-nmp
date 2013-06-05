1320ebb1681a8a41986e5d910336c3fa8bfb7063
liuguangzhao <liuguangzhao@users.sf.net>
2013-06-05 17:15:00 +0000

    ext

--- build_php.sh
+++ build_php.sh
@@ -12,7 +12,7 @@ set -x
 #CC="$ACC" LD=$ALD CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.4.15 --enable-fpm --without-pear --without-iconv --disable-libxml --disable-xml --disable-xmlreader --disable-xmlwriter --disable-simplexml --disable-dom          --disable-posix   --target=arm-linux --host=x86_64;
    
 EDEFS="-DLC_CTYPE=0 -DLC_NUMERIC=1 -DLC_TIME=2 -DLC_COLLATE=3 -DLC_MONETARY=4 -DLC_MESSAGES=5 -DLC_ALL=6 -DLC_PAPER=7 -DLC_NAME=8 -DLC_ADDRESS=9 -DLC_TELEPHONE=10 -DLC_MEASUREMENT=11 -DLC_IDENTIFICATION=12";
-CC="$ACC" LD=$ALD LDFLAGS="-lc" CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.4.15 --disable-all --enable-fpm --enable-json --enable-zlib --enable-zip --enable-pdo  --with-pdo-sqlite3 --enable-sockets --enable-bcmath --enable-mbstring --enable-mbregex  --target=arm-linux --host=x86_64;
+CC="$ACC" LD=$ALD LDFLAGS="-lc" CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.4.15 --disable-all --enable-fpm --enable-json --enable-zlib --enable-zip --enable-pdo  --with-pdo-sqlite3 --with-sqlite3 --enable-sockets --enable-bcmath --enable-mbstring --enable-mbregex  --target=arm-linux --host=x86_64;
 
 ## #undef HAVE_LOCALE_H
 ## #undef HAVE_SETLOCALE
@@ -42,7 +42,10 @@ if [ -f main/php_config.h ] ; then
     echo "#define LC_ALL 6"  >> main/php_config.h
     echo "#undef HAVE_LOCALE_H" >> main/php_config.h
 #    echo "#undef HAVE_SETLOCALE" >> main/php_config.h
+
+    # echo "# define fdatasync fsync" >> main/php_config.h
 fi
+
 
 
 cd -;
