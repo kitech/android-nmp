82da1c84be60d5022d1d13a89973c9afa9d4a117
liuguangzhao <liuguangzhao@users.sf.net>
2013-07-11 09:30:02 +0000

    abcefg

--- build_php.sh
+++ build_php.sh
@@ -4,7 +4,7 @@ TOP_DIR=$(pwd);
 
 source ./settings.sh;
 
-cd php-5.4.15;
+cd php-5.5.0;
 export CC=$ACC
 echo $CC;
 # exit;
@@ -12,7 +12,9 @@ set -x
 #CC="$ACC" LD=$ALD CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.4.15 --enable-fpm --without-pear --without-iconv --disable-libxml --disable-xml --disable-xmlreader --disable-xmlwriter --disable-simplexml --disable-dom          --disable-posix   --target=arm-linux --host=x86_64;
    
 EDEFS="-DLC_CTYPE=0 -DLC_NUMERIC=1 -DLC_TIME=2 -DLC_COLLATE=3 -DLC_MONETARY=4 -DLC_MESSAGES=5 -DLC_ALL=6 -DLC_PAPER=7 -DLC_NAME=8 -DLC_ADDRESS=9 -DLC_TELEPHONE=10 -DLC_MEASUREMENT=11 -DLC_IDENTIFICATION=12";
-CC="$ACC" LD=$ALD LDFLAGS="-lc" CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.4.15 --disable-all --enable-fpm --enable-json --enable-zlib --enable-zip --enable-pdo  --with-pdo-sqlite3 --with-sqlite3 --enable-sockets --enable-bcmath --enable-mbstring --enable-mbregex  --target=arm-linux --host=x86_64;
+CC="$ACC" LD=$ALD LDFLAGS="-lc" CFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt" ./configure --prefix=/sdcard/serv/stow/php-5.5.0 --disable-all --enable-fpm --enable-json --with-zlib --enable-zip --enable-pdo --with-sqlite3 --enable-sockets --enable-bcmath --enable-mbstring --enable-mbregex --target=arm-linux --host=x86_64;
+
+### next --with-mcrypt
 
 ## #undef HAVE_LOCALE_H
 ## #undef HAVE_SETLOCALE
--- readme.txt
+++ readme.txt
@@ -5,4 +5,8 @@ android-nginx:
 *) nginx.conf配置，需要user root;
 *) 编译时需要修改几个文件，
 *) 启动时需要把nginx放到/data/local/tmp/nginx目录
+*) 或者把/sdcard/重新挂载，支持rw挂载参数，mount -o remount,rw /mnt/sdcard
+*) ifconfig wlan0 当前IP。
+
+
 
