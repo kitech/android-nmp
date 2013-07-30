47b85dc3d34feadd43a109bf67e8d4c1359a1f59
liuguangzhao <liuguangzhao@users.sf.net>
2013-07-30 13:23:43 +0000

    uppped

--- build_mysql.sh
+++ build_mysql.sh
@@ -17,12 +17,21 @@ EDEFS="-DLC_CTYPE=0 -DLC_NUMERIC=1 -DLC_TIME=2 -DLC_COLLATE=3 -DLC_MONETARY=4 -D
 # CC="$ACC" LD=$ALD LDFLAGS="-lc -L$INS_DIR/ncurses-5.9/lib" CXXFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt -I$INS_DIR/ncurses-5.9/include/ncurses" ./configure --prefix=/sdcard/serv/stow/mysql-5.1.70 --with-pthread --with-atomic-ops=rwlocks --target=arm-linux --host=x86_64;
 
 
-CC="$ACC" CXX="$ACXX" LD=$ALD LDFLAGS="-lc -L$INS_DIR/ncurses-5.9/lib -lncurses -L$INS_DIR/readline-6.2/lib -lreadline" CXXFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt -I$INS_DIR/ncurses-5.9/include/ncurses -I$INS_DIR/readline-6.2/include" cmake ../mysql-5.5.32/ -DCMAKE_VERBOSE_MAKEFILE=on -DWITH_FEDERATED_STORAGE_ENGINE=off -DWITH_UNIT_TESTS=off
+rm -vf CMakeCache.txt
+CC="$ACC" CXX="$ACXX" LD=$ALD LDFLAGS="-lc -L$INS_DIR/ncurses-5.9/lib -lncurses $INS_DIR/ncurses-5.9/lib/libncurses.a -L$INS_DIR/readline-6.2/lib -lreadline $INS_DIR/readline-6.2/lib/libreadline.a" CXXFLAGS="-DANDROID -DNGX_SYS_NERR=1 -DIOV_MAX=16 $EDEFS_123 -I$TOP_DIR/compat -I$TOP_DIR/compat/libcrypt -I$INS_DIR/ncurses-5.9/include/ncurses -I$INS_DIR/readline-6.2/include" cmake ../mysql-5.5.32/ -DCMAKE_VERBOSE_MAKEFILE=on -DWITH_FEDERATED_STORAGE_ENGINE=off -DWITH_UNIT_TESTS=off
 
+
+ESC_INS_DIR=$(echo $INS_DIR | sed 's/\//\\\//g')
+
+sed -i "s/o mysql/o mysql $ESC_INS_DIR\/readline-6.2\/lib\/libreadline.a $ESC_INS_DIR\/ncurses-5.9\/lib\/libncurses.a/g"   client/CMakeFiles/mysql.dir/link.txt
 sed -i 's/\.\/comp_err/qemu-arm \.\/comp_err/g' extra/CMakeFiles/GenError.dir/build.make
 # comp_sql
 sed -i 's/\.\/gen_lex_hash/qemu-arm \.\/gen_lex_hash/g' sql/CMakeFiles/sql.dir/build.make
 sed -i 's/\.\/gen_lex_hash/qemu-arm \.\/gen_lex_hash/g' sql/CMakeFiles/GenServerSource.dir/build.make
+
+SED_SRC=`pwd`/scripts/comp_sql
+ESC_SED_SRC=$(echo $SED_SRC | sed 's/\//\\\//g')
+sed -i "s/$ESC_SED_SRC/qemu-arm $ESC_SED_SRC/g" scripts/CMakeFiles/GenFixPrivs.dir/build.make
 
 
 # ushort的问题出现的比较多。
